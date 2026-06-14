be0b419c39acbb0e5de61b37ed1f5a4fbf2e1b6a
Ayusht20 <ayushtrilokchandani2005@gmail.com>
2026-06-15 00:04:56 +0530

    Added important files for register API along with schemas..

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
-from app.config import settings
-from app.database import engine ,Base
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
 
+from app.config import settings
+from app.database import engine ,Base,get_db
+from app.schemas import UserCreate,UserResponse
 from app.models.user import User
 
 Base.metadata.create_all(bind=engine)
@@ -11,3 +13,24 @@ app =FastAPI(title=settings.PROJECT_NAME)
 @app.get("/")
 def read_root():
     return {"message": f"Welcome to the restarted {settings.PROJECT_NAME}!"}
+
+
+@app.post("/api/auth/register",response_model=UserResponse,status_code=status.HTTP_201_CREATED)
+def register(user_data:UserCreate,db:Session=Depends(get_db)):
+    existing_user=db.query(User).filter(User.email==user_data.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered!!"
+        )
+    new_user=User(
+        name=user_data.name,
+        email=user_data.email,
+        hashed_password=user_data.password
+    )
+    db.add(new_user)
+    db.commit()
+
+    db.refresh(new_user)
+
+    return new_user
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from app.database import Base 
 
 class User(Base):
-    __tablename__="user"
+    __tablename__="users"
 
     id=Column(Integer,primary_key=True,index=True)
     name=Column(String,nullable=False)
     email=Column(String,unique=True,index=True,nullable=False)
-    hashed_paswword=Column(String,nullable=False)
+    hashed_password=Column(String,nullable=False)
     created_at=Column(DateTime,default=datetime.utcnow)
 
 
